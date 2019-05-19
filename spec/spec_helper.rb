491685cbc0f9926c4164999370db111917bdf141
David Hrachovy <david.hrachovy@gmail.com>
2019-05-19 15:32:56 +0200

    Fix code coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,8 @@
-require 'webmock/rspec'
 require 'simplecov'
 require 'coveralls'
-require 'sms_manager'
-
-SimpleCov.formatters = [SimpleCov::Formatter::HTMLFormatter, Coveralls::SimpleCov::Formatter]
 SimpleCov.start
+
+require 'webmock/rspec'
+require 'sms_manager'
 
 WebMock.disable_net_connect!
