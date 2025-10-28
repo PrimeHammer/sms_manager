f7457e5397633fb3dc2dd275fcd242d849b6358f
Filip Zachar <tulak45@gmail.com>
2025-10-28 20:29:04 +0100

    Include error codes in message

--- lib/sms_manager/error/sending_error.rb
+++ lib/sms_manager/error/sending_error.rb
@@ -13,9 +13,9 @@ module SmsManager
       if code >= 900 && code <= 999
         CODE_9XX
       elsif CODES[code]
-        CODES[code]
+        "#{CODES[code]} (#{code})"
       else
-        'Neznámý chybový kód'
+        "Neznámý chybový kód (#{code})"
       end
     end
 
@@ -25,6 +25,7 @@ module SmsManager
       103 => 'Neplatné uživatelské jméno nebo heslo',
       104 => 'Neplatný parametr gateway',
       105 => 'Nedostatek kreditu pro prepaid',
+      109 => 'Požadavek neobsahuje všechna vyžadovaná data',
       201 => 'Žádná platná telefonní čísla v požadavku',
       202 => 'Text zprávy neexistuje nebo je příliš dlouhý',
       203 => 'Neplatný parametr sender (odesílatele nejprve nastavte ve webovém rozhraní)'
